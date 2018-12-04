44451782b3830bc0a0fcc4d0816ccc969412d381
Jordan Willis <jordanwillis@Hal12k.local>
2018-12-03 22:22:37 -0800

    new functions for hellper

--- pyrosetta_helper/helper_functions.py
+++ pyrosetta_helper/helper_functions.py
@@ -3,6 +3,7 @@ from Bio.PDB import PDBParser, PDBIO
 from Bio.SubsMat import MatrixInfo as matlist
 from Bio import Seq
 from Bio import pairwise2
+import math
 import Bio
 import pandas
 import array
@@ -73,47 +74,10 @@ def pose_structure_df(pose, display_residues=[]):
     df = pandas.DataFrame(return_list)
     return df.set_index('Pose')
 
-def get_per_residue_energies_df(p,scorefxn):
-    for_pandas = []
-    scorefxn(p)
-    score_type_dataframe = {}
-    #pandasDataFrame()
-    weights_map = scorefxn.weights()
-    energy = p.energies()
-    
-    for s in range(1, int(rosetta.core.scoring.end_of_score_type_enumeration)+ 1):
-        score_type = rosetta.core.scoring.ScoreType(s)
-        score_weight = weights_map[score_type]
-        if score_weight:
-           score_type_dataframe[score_type] = score_weight
- 
-    for resi in range(1,p.total_residue()+1):
-        raw_score_total = 0.0
-        total_score = 0.0
-        for score_term in score_type_dataframe:
-            st = score_term
-            w = score_type_dataframe[score_term]
-            score = energy.residue_total_energies(resi)[score_term]
-            wt_score = w * score
-            for_pandas.append({
-             'Pose':resi,
-             'ScoreType':str(st).split('.')[-1],
-             'Weight': w,
-             'RawScore': score,
-             'Score':wt_score})
-            raw_score_total += score
-            total_score += wt_score
-        for_pandas.append({
-             'Pose':resi,
-             'ScoreType':'total',
-             'Weight': 0,
-             'RawScore': raw_score_total,
-             'Score':total_score})
-
-    df = pandas.DataFrame(for_pandas)[['Pose','ScoreType','Weight','RawScore','Score']]
-    return df
-
 def compress_file(file_):
+    '''
+        Zips a file in place
+    '''
     import gzip
     import shutil
     import os
@@ -121,6 +85,63 @@ def compress_file(file_):
     with open(file_, 'rb') as f_in, gzip.open(file_+'.gz', 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
     os.remove(file_)
+
+def score_pose_to_df(input_pose, score_function='ref2015'):
+    '''
+        Add score information to pose dataframe
+    '''
+    ###Pose structure dataframe from 
+    pose_df = pose_structure_df(input_pose)
+    ref2015_sf = create_score_function(score_function)
+    ref2015_sf(input_pose)
+    energies = input_pose.energies()
+    residue_energies = [energies.residue_total_energy(i) for i in range(1, input_pose.total_residue() + 1)]
+    pose_df['residue_energy'] = residue_energies
+
+    weights = [pyrosetta.rosetta.core.scoring.ScoreType(s)
+        for s in range(1, int(
+            pyrosetta.rosetta.core.scoring.end_of_score_type_enumeration) + 1)
+        if ref2015_sf.weights()[pyrosetta.rosetta.core.scoring.ScoreType(s)]]
+
+    per_residue_unwiehgts = energies.residue_total_energies
+    per_residue_weighted = []
+    for residue_index in range(1,input_pose.total_residue() + 1):
+        sum_ = 0.0
+        for weight in weights:
+        #print(weight)
+            entry = {'Pose': residue_index,
+             'score_type': str(weight).split('.')[-1],
+         'score': per_residue_unwiehgts(residue_index)[weight] * ref2015_sf.weights()[weight]}
+            per_residue_weighted.append(
+            entry)
+    per_res_df = pandas.DataFrame(per_residue_weighted)
+    scored_info_df = pose_df.join(per_res_df.pivot('Pose','score_type','score'))
+    return scored_info_df
+
+def get_sphere_sasa(input_pose):
+    '''return a list of all residues per residue sasa'''
+    neighbor_counts = []
+    p = input_pose
+    num_residues = p.total_residue()
+    for res_target in range(1,num_residues+1):
+        neighbor_count = 0.0
+        CB_atom = "CB"
+        ##If glycine, then use 1HA 
+        if p.residue(res_target).type().name1() == 'G':
+            CB_atom = "1HA"
+        for res_neighbor in range(1, num_residues+1):
+            ##Dont measure if res_neighbor and target is self
+            if res_neighbor == res_target:
+                continue
+            else:
+                neighbor_atom = "CB"
+                if p.residue(res_neighbor).type().name1() == 'G':
+                    neighbor_atom = "1HA"
+            distance = p.residue(res_target).xyz(CB_atom).distance(
+                p.residue(res_neighbor).xyz(neighbor_atom))
+            neighbor_count += 1.0/(1.0 + math.exp(1.0*(distance-9.0)))
+        neighbor_counts.append(neighbor_count)
+    return neighbor_counts
 
 
 
